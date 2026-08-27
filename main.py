f31a90b8392317f569dca60c53fcddafea3c4a9b
Rossyn <rossyn.tasaka@gmail.com>
2026-08-26 17:20:00 -1000

    second

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-def main():
-    print(f"Starting Asteroids")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -22,7 +19,9 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
