756be5d723d8457ba61aff1cc717a6f420506c7f
Rossyn <rossyn.tasaka@gmail.com>
2026-08-29 15:03:00 -1000

    Ch 3 Les 3 rev 1

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
