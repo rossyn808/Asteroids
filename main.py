6d1747e0978a58268f7edb1eb59bbaf373f7f51b
Rossyn <rossyn.tasaka@gmail.com>
2026-09-02 18:25:03 -1000

    Ch4 Les3 rev 3

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state, log_event
 from player import Player
+from shot import Shot
 
 
 def main() -> None:
